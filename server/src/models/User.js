a5e3994f3bb10129f4fd4b027938f6416bdc7da4
Mrunal Awate <mrunalawate0tv@gmail.com>
2026-09-13 02:31:13 +0530

    adding admin page

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true, trim: true },
     email: { type: String, required: true, unique: true, lowercase: true, trim: true },
     passwordHash: { type: String, required: true },
-    isAdmin: { type: Boolean, default: false },
+    isAdmin: { type: Boolean, default: true },
   },
   { timestamps: true }
 );
